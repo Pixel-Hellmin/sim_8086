11ffc7fffed3f3b21b0d1e4e8fc472983f3345ff
Fermin <ferminoloriz@gmail.com>
2023-03-03 19:12:45 -0600

    Changed from match logic to table

--- src/main.rs
+++ src/main.rs
@@ -6,31 +6,22 @@ fn main() {
     let loaded_bytes: Vec<u8> = fs::read(&args[1])
         .expect("Error reading file");
 
+    static REGISTER_TABLE:[[&str; 2]; 8] = [["al", "ax"], ["cl", "cx"], ["dl", "dx"], ["bl", "bx"], ["ah", "sp"], ["ch", "bp"], ["dh", "si"], ["bh", "di"], ];
     let mut result = String::new();
 
     for instruction in (0..loaded_bytes.len()).step_by(2) {
-        println!("instructions {}", instruction);
-        
         let mut result_operation = &"";
 
         let first_byte = loaded_bytes[instruction];
-        println!("first byte {}", first_byte);
         // NOTE(Fermin) 0b00100010 == mov == 34, in decimal
         let operation = (first_byte >> 2) & 0b00111111;
-        println!("operation {}", operation);
         let d = (first_byte >> 1) & 0b00000001; 
-        println!("d {}", d);
         let w = first_byte & 0b00000001;
-        println!("w {}", w);
 
         let second_byte = loaded_bytes[instruction+1];
-        println!("second byte {}", second_byte);
         let mod_field = (second_byte >> 6) & 0b00000011;
-        println!("mod {}", mod_field);
         let reg = (second_byte >> 3) & 0b00000111;
-        println!("reg {}", reg);
         let r_m = second_byte & 0b00000111;
-        println!("r_m {}", r_m);
 
         if operation == 34 { result_operation = &"mov"; } 
         else { 
@@ -42,66 +33,10 @@ fn main() {
             break;
         } 
 
-        let reg_res: &str = match reg {
-            0b00000000 => {
-                if w == 0b00000000 {"al"} else {"ax"}
-            },
-            0b00000001 => {
-                if w == 0b00000000 {"cl"} else {"cx"}
-            },
-            0b00000010 => {
-                if w == 0b00000000 {"dl"} else {"dx"}
-            },
-            0b00000011 => {
-                if w == 0b00000000 {"bl"} else {"bx"}
-            },
-            0b00000100 => {
-                if w == 0b00000000 {"ah"} else {"sp"}
-            },
-            0b00000101 => {
-                if w == 0b00000000 {"ch"} else {"bp"}
-            },
-            0b00000110 => {
-                if w == 0b00000000 {"dh"} else {"si"}
-            },
-            0b00000111 => {
-                if w == 0b00000000 {"bh"} else {"di"}
-            },
-            _ => "reg not found"
-        };
-
-        let r_m_res: &str = match r_m {
-            0b00000000 => {
-                if w == 0b00000000 {"al"} else {"ax"}
-            },
-            0b00000001 => {
-                if w == 0b00000000 {"cl"} else {"cx"}
-            },
-            0b00000010 => {
-                if w == 0b00000000 {"dl"} else {"dx"}
-            },
-            0b00000011 => {
-                if w == 0b00000000 {"bl"} else {"bx"}
-            },
-            0b00000100 => {
-                if w == 0b00000000 {"ah"} else {"sp"}
-            },
-            0b00000101 => {
-                if w == 0b00000000 {"ch"} else {"bp"}
-            },
-            0b00000110 => {
-                if w == 0b00000000 {"dh"} else {"si"}
-            },
-            0b00000111 => {
-                if w == 0b00000000 {"bh"} else {"di"}
-            },
-            _ => "reg not found"
-        };
-
         if d == 0b00000001 {
-            result += &format!("{} {}, {}\n", result_operation, reg_res, r_m_res);
+            result += &format!("{} {}, {}\n", result_operation, REGISTER_TABLE[reg as usize][w as usize], REGISTER_TABLE[r_m as usize][w as usize]);
         } else {
-            result += &format!("{} {}, {}\n", result_operation, r_m_res, reg_res);
+            result += &format!("{} {}, {}\n", result_operation, REGISTER_TABLE[r_m as usize][w as usize], REGISTER_TABLE[reg as usize][w as usize]);
         }
 
         /*
