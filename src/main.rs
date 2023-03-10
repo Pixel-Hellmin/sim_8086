dec31883121ccf07a50fd1050870ee3ba2d8b70c
Fermin <ferminoloriz@gmail.com>
2023-03-10 11:46:51 -0600

    Changed from for loop to while loop to move index depending on instruction

--- src/main.rs
+++ src/main.rs
@@ -20,8 +20,9 @@ fn main() {
 
     println!("bits 16\n");
 
-    for instruction in (0..loaded_bytes.len()).step_by(2) {
-        let first_byte = loaded_bytes[instruction];
+    let mut index = 0;
+    while index < loaded_bytes.len() {
+        let first_byte = loaded_bytes[index];
         let op = match (first_byte >> 2) & 0b00111111 {
             34 => &"mov",
             _  => &"?uop", // NOTE(Fermin): unidentified op
@@ -29,7 +30,7 @@ fn main() {
         let d = (first_byte >> 1) & 0b00000001; 
         let w = first_byte & 0b00000001;
 
-        let second_byte = loaded_bytes[instruction+1];
+        let second_byte = loaded_bytes[index+1];
         let mod_field = (second_byte >> 6) & 0b00000011;
         let reg = (second_byte >> 3) & 0b00000111;
         let r_m = second_byte & 0b00000111;
@@ -43,5 +44,6 @@ fn main() {
         } else {
             println!("{} {}, {}", op, REGISTER_TABLE[r_m as usize][w as usize], REGISTER_TABLE[reg as usize][w as usize]);
         }
+        index += 2;
     }
 }
