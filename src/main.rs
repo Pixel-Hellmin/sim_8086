1628ffb7a59713063b3a5dc018d0f485f5b565dc
Fermin <ferminoloriz@gmail.com>
2023-03-10 12:03:44 -0600

    Iterate each byte and manually move index to next instruction

--- src/main.rs
+++ src/main.rs
@@ -23,9 +23,17 @@ fn main() {
     let mut index = 0;
     while index < loaded_bytes.len() {
         let first_byte = loaded_bytes[index];
+        let mut index_increment = 0;
         let op = match (first_byte >> 2) & 0b00111111 {
-            34 => &"mov",
-            _  => &"?uop", // NOTE(Fermin): unidentified op
+            34 => {
+                // NOTE(Fermin): [100010 d w]
+                index_increment += 2;
+                &"mov"
+            },
+            _  => {
+                index_increment += 2;
+                &"?uop" // NOTE(Fermin): unidentified op
+            },
         };
         let d = (first_byte >> 1) & 0b00000001; 
         let w = first_byte & 0b00000001;
@@ -35,8 +43,19 @@ fn main() {
         let reg = (second_byte >> 3) & 0b00000111;
         let r_m = second_byte & 0b00000111;
 
-        if mod_field != 3 { 
-            println!("NOTE: not reg to reg op. Unsupported");
+        match mod_field { 
+            0 => {
+                if r_m == 6 {
+                    index_increment += 2;
+                }
+            },
+            1 => {
+                index_increment += 1;
+            },
+            2 => {
+                index_increment += 2;
+            },
+            _ => (),
         } 
 
         if d == 0b00000001 {
@@ -44,6 +63,7 @@ fn main() {
         } else {
             println!("{} {}, {}", op, REGISTER_TABLE[r_m as usize][w as usize], REGISTER_TABLE[reg as usize][w as usize]);
         }
-        index += 2;
+
+        index += index_increment;
     }
 }
